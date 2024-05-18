8b5a1ee620c698c54ed26c244a8cba6f6fb3e872
Fraser Duncan <fduncn@gmail.com>
2024-05-18 13:31:54 +0100

    made login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [isSignUp,setIsSignUp]=useState(false)
+    return (
+        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+            <div className="flex items-center justify-center py-12">
+                <div className="mx-auto grid w-[350px] gap-6">
+                    <div className="grid gap-2 text-center">
+                        <h1 className="text-3xl font-bold">{!isSignUp?"Login":"Sign Up" }</h1>
+                        <p className="text-balance text-muted-foreground">
+                            {!isSignUp ? "Enter your username below to log in to your account" : "Enter a username below to create an account"}
+                        </p>
+                    </div>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="username">username</Label>
+                            <Input
+                                id="username"
+                                type="text"
+                                placeholder="HeeHeeHeeHaw182"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                        <Button type="submit" className="w-full">
+                            {!isSignUp ? "Login" : "Create Account"}
+                        </Button>
+                    </div>
+                    {
+                        !isSignUp?(
+                        <div className="mt-4 text-center text-sm">
+                            Don&apos;t have an account?{" "}
+                                <a onClick={() => setIsSignUp(true)} href="#" className="underline">
+                                Sign up
+                            </a>
+                            </div>
+                        ) : (
+                                <div className="mt-4 text-center text-sm">
+                                    Already have an account?{" "}
+                                    <a onClick={ ()=>setIsSignUp(false) } href="#" className="underline">
+                                        Log in
+                                    </a>
+                                </div>
+                        )
+                    }
+                </div>
+            </div>
+            <div className="hidden bg-muted lg:block">
+                <img
+                    src="https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80"
+                    alt="Image"
+                    width="1920"
+                    height="1080"
+                    className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+            </div>
+        </div>
+    )
 }
 
 export default App
