64edfa28cfad2e3b235aad0076c6b77eab1d13f9
Fraser Duncan <fduncn@gmail.com>
2024-05-19 11:56:52 +0100

    Implemented React Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-//import './index.css'
-import './globals.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import ErrorPage from "./error-page";
+import Root from "./routes/root";
+import Auth from "./routes/Auth"
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-        <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "auth",
+                element: <Auth/>,
+            },
+        ],
+    },
+]);
+//@ts-ignore
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
